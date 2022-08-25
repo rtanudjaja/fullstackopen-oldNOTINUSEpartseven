85a46954dd1e7e8a7147ed801f4ad1e163ca7114
Ricky Tanudjaja CT <ricky.tanudjaja@cartrack.com>
2022-08-25 13:36:08 +0700

    finish ultimate hooks

--- ultimate-hooks/src/App.js
+++ ultimate-hooks/src/App.js
@@ -17,11 +17,19 @@ const useField = (type) => {
 
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
+  
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(baseUrl)
+      setResources(response.data)
+    }
+    fetchData()
+  }, [baseUrl])
 
-  // ...
-
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources([...resources, response.data])
+    return response.data
   }
 
   const service = {
